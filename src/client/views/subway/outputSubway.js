992c2154b4b1a58d48d14aef7e50106228b2f7d0
BOUKOUFALLAH Brahim <boukoufallah.brahim@gmail.com>
2014-08-26 10:53:23 +0200

    link stations between them

--- src/client/views/subway/outputSubway.js
+++ src/client/views/subway/outputSubway.js
@@ -11,16 +11,40 @@ Template.outputSubway.rendered = function() {
 
     if(! self.handle) {
         self.handle = Deps.autorun(function() {
+            var stations = Items.find({projectId: self.data._id}).fetch();
+
             var moveStation = function(station) {
                 d3.select(this)
                   .attr("cy", d3.event.y)
                   .attr("cx", d3.event.x)
                 ;
+                link
+                    .attr("x1", function(station, index) {
+                        return station.options.subway.cx;
+                    })
+                    .attr("y1", function(station, index) {
+                        return station.options.subway.cy;
+                    })
+                    .attr("x2", function(station, index) {
+                        if(stations[index+1] !== undefined) {
+                            return stations[index+1].options.subway.cx;
+                        } else {
+                            return station.options.subway.cx;
+                        }
+                    })
+                    .attr("y2", function(station, index) {
+                        if(stations[index+1] !== undefined) {
+                            return stations[index+1].options.subway.cy;
+                        } else {
+                            return station.options.subway.cy;
+                        }
+                    })
+                ;
             }
 
             var drag = d3.behavior.drag()
                 .on("drag", moveStation);
-            console.log(drag);
+
             d3.select("#subway-legend > ul").selectAll('li').data(ItemCategories.find({projectId: self.data._id}).fetch()).enter()
                 .append("li")
                     .attr("id", function(line) {
@@ -36,11 +60,27 @@ Template.outputSubway.rendered = function() {
                         })
             ;
 
-            d3.select("#output > svg").selectAll('circle').data(Items.find({projectId: self.data._id}).fetch()).enter()
+             var link = d3.select("#output > svg").selectAll(".line")
+               .data(stations, function(station, index) {
+                    return index;
+                })
+               .enter()
+               .append("line")
+                .attr("fill", "none")
+                .style("stroke", "rgb(6,120,155)")
+            ;
+
+            d3.select("#output > svg").selectAll('circle').data(stations).enter()
                 .append('circle')
                     .attr('id', function(station) {
                         return station._id;
                     })
+                .on("mouseout", function(station){
+                    station.options.subway.cx = d3.select(this).attr("cx");
+                    station.options.subway.cy = d3.select(this).attr("cy");
+                    console.log(station.options.subway);
+                    ;
+                })
                     .attr('data-name', function(station) {
                         return station.name;
                     })
@@ -54,8 +94,12 @@ Template.outputSubway.rendered = function() {
                         return station.options.subway.cy;
                     })
                     .attr("class", "subway-station")
+                    .text(function(station) {
+                        return station.name;
+                    })
                     .call(drag)
             ;
+
         });
     }
 };

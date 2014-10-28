4832af2f3cd7f27b7fc1e7100fc34b5f37840513
BOUKOUFALLAH Brahim <boukoufallah.brahim@gmail.com>
2014-10-28 09:51:40 +0100

    Write svg element directly in html file

--- src/client/templates/subway/output_subway.js
+++ src/client/templates/subway/output_subway.js
@@ -3,17 +3,3 @@ Template.outputSubway.helpers({
         return ItemCategories.find({projectId: this._id});
     }
 });
-
-Template.outputSubway.rendered = function() {
-
-    var svg = d3.select('#output')
-        .append('svg')
-    ;
-    svg.append('defs');
-    svg.append('g')
-        .attr('class', 'subway-background');
-    svg.append('g')
-        .attr('id', 'subway-lines');
-    svg.append('g')
-        .attr('id', 'subway-stations');
-}

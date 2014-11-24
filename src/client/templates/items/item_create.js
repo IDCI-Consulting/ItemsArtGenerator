ba82c778348184715d02073d1a1533b5cda50663
BOUKOUFALLAH Brahim <boukoufallah.brahim@gmail.com>
2014-11-24 17:17:59 +0100

    Display a error message if a category is unchecked

--- src/client/templates/items/item_create.js
+++ src/client/templates/items/item_create.js
@@ -8,18 +8,24 @@ Template.itemCreate.events({
     'submit form': function(e, template) {
         e.preventDefault();
 
-        var formData = $(e.target).serializeArray(),
-            item = {
-                categories: [],
-            };
+        var formData = $(e.target).serializeArray();
+        var item = {
+            categories: [],
+        };
 
         var boundData = Meteor.bindFormData(item, formData);
         var itemId = Items.insert(boundData);
 
-        _.each(boundData.categories, function(categoryId) {
-            var category = ItemCategories.findOne(categoryId);
-            Meteor.addCategoryItem(category, itemId);
-            ItemCategories.update(category._id, {$set: {"items": category.items}});
-        });
+        if (boundData.categories.length === 0) {
+            $("p.error").replaceWith('<p class="error">You must choose a category</p>');
+
+        } else {
+            _.each(boundData.categories, function(categoryId) {
+                var category = ItemCategories.findOne(categoryId);
+                Meteor.addCategoryItem(category, itemId);
+                ItemCategories.update(category._id, {$set: {"items": category.items}});
+            });
+            $('#modalEditor').foundation('reveal', 'close');
+        }
     }
 });

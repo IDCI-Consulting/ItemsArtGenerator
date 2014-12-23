cea5057df80e69cefab910a5625a9f3e18c43eb0
BOUKOUFALLAH Brahim <boukoufallah.brahim@gmail.com>
2014-12-23 09:25:17 +0100

    remove fixtures

--- src/server/fixtures.js
+++ src/server/fixtures.js
@@ -1,16 +1,1 @@
-if (Projects.find().count() === 0) {
-    // insert data in projects collection if there's no data
-    var myjson = JSON.parse(Assets.getText("data.json"));
 
-    _.each(myjson.projects, function(value, key) {
-        Projects.insert(value);
-    });
-
-    _.each(myjson.categories, function(value, key) {
-        ItemCategories.insert(value);
-    });
-
-    _.each(myjson.items, function(value, key) {
-        Items.insert(value);
-    });
-}

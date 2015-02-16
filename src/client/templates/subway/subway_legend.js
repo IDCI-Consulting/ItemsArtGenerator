5724c2abff8ff05d5f2f4f70732dff6a4dc9a604
BOUKOUFALLAH Brahim <boukoufallah.brahim@gmail.com>
2015-02-16 09:55:21 +0100

    Display legend raw in alphabetics order

--- src/client/templates/subway/subway_legend.js
+++ src/client/templates/subway/subway_legend.js
@@ -1,5 +1,5 @@
 Template.subwayLegend.helpers({
     lines: function() {
-        return ItemCategories.find({projectId: this._id});
+        return ItemCategories.find({projectId: this._id}, {sort: { name: 1 }});
     }
 });
